c7928c2b53120882096696db34a755a7d6df52ad
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-07-15 20:03:19 -0400

    work on css cleanup

--- js/main.js
+++ js/main.js
@@ -112,40 +112,42 @@ function sliderChange(val) {
 
 
 
-
-
-/* Please ❤ this if you like it! */
-
-
-(function($) { "use strict";
+// window.addEventListener('load', function () {
+//   (function($) { "use strict";
 
 	
-	//Navigation
+// 	//Navigation
 
-	var app = function () {
-		var body = undefined;
-		var menu = undefined;
-		var menuItems = undefined;
-		var init = function init() {
-      body = document.querySelector('body');
-      backtotop = document.getElementById('backtotop');
+// 	var app = function () {
+// 		var body = undefined;
+// 		var menu = undefined;
+// 		var menuItems = undefined;
+// 		var init = function init() {
+//       body = document.querySelector('body');
+//       backtotop = document.getElementById('backtotop');
 
-			menu = document.querySelector('.menu-icon');
-			menuItems = document.querySelectorAll('.nav__list-item');
-			applyListeners();
-		};
-		var applyListeners = function applyListeners() {
-			menu.addEventListener('click', function () {
-        toggleClass(backtotop, 'visible')
-				return toggleClass(body, 'nav-active');
-			});
-		};
-		var toggleClass = function toggleClass(element, stringClass) {
-			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
-		};
-		init();
-	}();
+// 			menu = document.querySelector('.menu-icon');
+// 			menuItems = document.querySelectorAll('.nav__list-item');
+// 			applyListeners();
+// 		};
+// 		var applyListeners = function applyListeners() {
+// 			menu.addEventListener('click', function () {
+//         toggleClass(backtotop, 'visible')
+// 				return toggleClass(body, 'nav-active');
+// 			});
+// 		};
+// 		var toggleClass = function toggleClass(element, stringClass) {
+// 			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+// 		};
+// 		init();
+// 	}
 
 	
 	
-})(jQuery); 
+// })(jQuery); 
+
+// })
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("working")
+});
